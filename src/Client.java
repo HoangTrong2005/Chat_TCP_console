6156284755f47bf2ce34db192b3cd78d95609899
HoangTrong2005 <hoangphuoctrong0@gmail.com>
2025-11-09 21:12:05 +0700

    Update Client.java
    Client kết nối vào server

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,21 @@
-package test;
+// Client kết nối vào server
+
+import java.io.IOException;
+import java.net.Socket;
 
 public class Client {
+    private final String SERVER_IP = "localhost";
+    private final int SERVER_PORT = 1433;
 
+    public Client() {
+        try (Socket socket = new Socket(SERVER_IP, SERVER_PORT)) {
+            System.out.println(" Đã kết nối tới server: " + SERVER_IP + ":" + SERVER_PORT);
+        } catch (IOException e) {
+            System.out.println(" Lỗi kết nối tới server: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        new Client();
+    }
 }
