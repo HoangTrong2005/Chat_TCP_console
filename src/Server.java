1f4ef95af4d0618b95e2d8ac73ae155a66fd69cb
HoangTrong2005 <hoangphuoctrong0@gmail.com>
2025-11-09 21:08:52 +0700

    Update Server.java
    Server lắng nghe kết nối

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,25 @@
-package test;
+// Server lắng nghe kết nối
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+    private final int SERVER_PORT = 1433;
 
+    public Server() {
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+            System.out.println("✅ Server đang chạy tại cổng " + SERVER_PORT);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("🔌 Client mới kết nối: " + socket.getRemoteSocketAddress());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
 }
