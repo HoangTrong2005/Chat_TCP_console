80f356d547a5dada1f4afbb74809bcaf485a984d
sang86 <sangnguyen862003@gmail.com>
2025-11-17 11:52:52 +0700

    Server nhận và chat lại cho Client

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,6 +1,7 @@
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientHandler implements Runnable {
     private Socket socket;
@@ -14,15 +15,33 @@ public class ClientHandler implements Runnable {
         try (
                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)
-        ) {
+            ){
+                
             writer.println("Chào! Bạn đã kết nối thành công tới server!");
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println("Client gửi: " + line);
-                writer.println("Server nhận được: " + line);
+            // Nhận tin từ client
+            new Thread(() -> {
+                try {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println("Client gửi: " + line);
+                    }
+                } catch (IOException e) {
+                    System.out.println(" Client đã ngắt kết nối.");
+                }
+            }).start();
+
+            //Server nhập tin gửi xuống client
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+                String msg = sc.nextLine();
+                writer.println("Server: " + msg);
+
+                if (msg.equalsIgnoreCase("/exit"))
+                    break;
             }
+
         } catch (IOException e) {
-            System.out.println("⚠️ Client ngắt kết nối.");
+            System.out.println(" Lỗi trong ClientHandler.");
         }
     }
 }
